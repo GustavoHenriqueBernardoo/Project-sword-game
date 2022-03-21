5bf21da61a9d65c165e4512aa5127d51bd11f65a
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-03-21 12:57:20 -0300

    Creating functions in the JS file
    Created the event listener to the start game button, when clicked change
     from the start screen to the choose sword screen
    
    Created a loop in the chooseBtn and add another event listener to each
    one of the iteration. When clicked change the mouse icon to the a sword icon image

--- app.js
+++ app.js
@@ -6,11 +6,37 @@ const scoreEL = document.getElementById('score')
 const timeEL = document.getElementById('time')
 const messageEL = document.getElementById('message')
 
-console.log(chooseBtn)
-console.log(screensEL)
+if (window.performance) {
+  console.log('enter here')
+  screensEL.forEach(screen => screen.classList.remove('up'))
+}
+
+let score = 0
+let seconds = 0
 
 
-// play_game_btn.addEventListener('click', () => {
-//   screensEL.forEach(screen =>
-//     screen.classList.add('up'))
-// })
+
+play_game_btn.addEventListener('click', () => {
+  console.log(screensEL[0])
+  screensEL[0].classList.add('up')
+})
+
+chooseBtn.forEach(sword => {
+  sword.addEventListener('click', () => {
+    const img = sword.querySelector('img')
+    const src = img.getAttribute('src')
+    const alt = img.getAttribute('alt')
+    // document.body.style.cursor = 'url(./imgs/small.png), auto'
+    // screensEL[1].classList.add('up')
+    setInterval(createEnemy, 1000)
+    startGame()
+  })
+})
+
+function createEnemy() {
+
+}
+
+function startGame() {
+
+}
