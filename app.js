405484bf351404a1663504f39ab7c7b2c02c7e2e
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-03-21 11:22:22 -0300

    Finished CSS for now
    added the enemy img, and hard-coded the position of it for now, but the
    position will be set dynamic as random in JavaScript
    
    added a dead class to the enemy when the player clicked in the enemy,
    the enemy will disappear from the screen
    
    added a rounded border to the enemies
    
    And finally, begin to create the variable and bring the elements from
    the DOM to JavaScript, and now I will start coding the
    functionality to the game

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const play_game_btn = document.getElementById('start-game')
 const screensEL = document.querySelectorAll('.screen')
+const chooseBtn = document.querySelectorAll('.sword')
+const game_container = document.getElementById('game-container')
+const scoreEL = document.getElementById('score')
+const timeEL = document.getElementById('time')
+const messageEL = document.getElementById('message')
 
+console.log(chooseBtn)
 console.log(screensEL)
 
-screensEL[0].classList.add('up')
 
 // play_game_btn.addEventListener('click', () => {
 //   screensEL.forEach(screen =>
